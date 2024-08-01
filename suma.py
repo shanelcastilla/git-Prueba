aca8fa44846e97a8596d0b856278c85743efadce
shanel <shanelcastilla@gmail.com>
2024-08-01 16:42:46 -0500

    operacion de suma

---
+++ suma.py
@@ -0,0 +1,7 @@
+numero1 = int(input("primer numero : "))
+numero2 = int(input("segundo numero : "))
+
+
+operacion = numero1 + numero2
+
+print(f"el resulatdo total de {numero1} + {numero2} = {operacion}")
